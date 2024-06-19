426543f46a76095be840092d87d9f9ee2182f999
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-06-20 00:44:21 +0530

    designed new navbar

--- lib/core/shared/nav_bar.dart
+++ lib/core/shared/nav_bar.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:solnext/core/constants/dimensions.dart';
 import 'package:solnext/src/home/ui/home_screen.dart';
 import 'package:solnext/src/nft/ui/nft_screen.dart';
 import 'package:solnext/src/marketplace/ui/marketplace_screen.dart';
+
 class NavigationScreen extends StatefulWidget {
   const NavigationScreen({super.key});
 
@@ -19,24 +21,42 @@ class _NavigationScreenState extends State<NavigationScreen> {
   }
 
   List _pages = [
-
-HomeScreen(),
-NftScreen(),
-MarketplaceScreen(),
-];  @override
+    HomeScreen(),
+    NftScreen(),
+    MarketplaceScreen(),
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _pages[_selectedTab],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedTab,
-        onTap: (index) {
-          _changeTab(index);
-        },
-        items: const [
-BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-BottomNavigationBarItem(icon: Icon(Icons.home), label: 'nft'),
-BottomNavigationBarItem(icon: Icon(Icons.home), label: 'marketplace'),
-        ],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), boxShadow: [
+          BoxShadow(
+            color: Color.fromARGB(0, 200, 200, 200).withOpacity(1),
+            offset: Offset(0, -5),
+            blurRadius: 22,
+            spreadRadius: -2,
+          )
+        ]),
+        margin: EdgeInsets.all(getScreenWidth(context) * 0.05),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: BottomNavigationBar(
+            elevation: 10,
+            currentIndex: _selectedTab,
+            onTap: (index) {
+              _changeTab(index);
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'nft'),
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'marketplace'),
+            ],
+          ),
+        ),
       ),
     );
   }
