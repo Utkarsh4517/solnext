d7e65717d6a75e832b7e3fcacec15870dea7fd57
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-06-20 00:55:18 +0530

    Added dynamic color to navbar

--- lib/core/shared/nav_bar.dart
+++ lib/core/shared/nav_bar.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:solnext/core/constants/dimensions.dart';
 import 'package:solnext/src/home/ui/home_screen.dart';
 import 'package:solnext/src/nft/ui/nft_screen.dart';
@@ -25,35 +28,52 @@ class _NavigationScreenState extends State<NavigationScreen> {
     NftScreen(),
     MarketplaceScreen(),
   ];
+
+  Widget _buildNavItem(String asset, int index) {
+    final color = _selectedTab == index ? Color(0xFF715AFF) : Color(0xFF838383);
+    return SvgPicture.asset(asset, width: 30, height: 30, color: color);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _pages[_selectedTab],
       bottomNavigationBar: Container(
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), boxShadow: [
-          BoxShadow(
-            color: Color.fromARGB(0, 200, 200, 200).withOpacity(1),
-            offset: Offset(0, -5),
-            blurRadius: 22,
-            spreadRadius: -2,
-          )
-        ]),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(0, 200, 200, 200).withOpacity(1),
+              offset: Offset(0, -5),
+              blurRadius: 22,
+              spreadRadius: -2,
+            )
+          ],
+        ),
         margin: EdgeInsets.all(getScreenWidth(context) * 0.05),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(20),
           child: BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
             elevation: 10,
             currentIndex: _selectedTab,
             onTap: (index) {
               _changeTab(index);
             },
-            items: const [
+            items: [
               BottomNavigationBarItem(
-                icon: Icon(Icons.home),
+                icon: _buildNavItem('assets/svgs/home_solnext1.svg', 0),
                 label: 'home',
               ),
-              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'nft'),
-              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'marketplace'),
+              BottomNavigationBarItem(
+                icon: _buildNavItem('assets/svgs/nft.svg', 1),
+                label: 'nft',
+              ),
+              BottomNavigationBarItem(
+                icon: _buildNavItem('assets/svgs/marketplace.svg', 2),
+                label: 'marketplace',
+              ),
             ],
           ),
         ),
