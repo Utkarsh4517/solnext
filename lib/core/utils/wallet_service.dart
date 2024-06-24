74fe2077ef54e001c0b527039c8fd54895ab9a01
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-06-24 22:50:43 +0530

    used secure storage to save mnemonics

--- lib/core/utils/wallet_service.dart
+++ lib/core/utils/wallet_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class WalletService {
@@ -10,5 +11,16 @@ class WalletService {
     final prefs = await SharedPreferences.getInstance();
     String publicKey = prefs.getString('publicKey')!;
     return publicKey;
+  }
+
+  static Future<void> saveMnemonics({required String mnemonics}) async {
+    final secureStorage = await FlutterSecureStorage();
+    secureStorage.write(key: 'mnemonics', value: mnemonics);
+  }
+
+  static Future<String> getMnemonics() async {
+    final secureStorage = await FlutterSecureStorage();
+    String? mnemonics = await secureStorage.read(key: 'mnemonics');
+    return mnemonics!;
   }
 }
--- lib/src/onBoarding/data/services/create_wallet.dart
+++ lib/src/onBoarding/data/services/create_wallet.dart
@@ -1,9 +1,11 @@
 import 'package:solana/solana.dart';
 import 'package:bip39/bip39.dart' as bip39;
+import 'package:solnext/core/utils/wallet_service.dart';
 
 class CreateWallet {
   static Future<(Ed25519HDKeyPair, String)> createNewWallet() async {
     final String mnemonic = bip39.generateMnemonic();
+    await WalletService.saveMnemonics(mnemonics: mnemonic);
     final Ed25519HDKeyPair keypair = await Ed25519HDKeyPair.fromMnemonic(mnemonic);
     return (keypair, mnemonic);
   }
